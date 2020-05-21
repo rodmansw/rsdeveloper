858e29f3b7ab8666d8924882ceb16a8fb1dd135e
Rodman Swanston <swanston.rod@gmail.com>
2020-05-21 16:34:26 -0500

    Fix config, Reduce image size

--- next.config.js
+++ next.config.js
@@ -1,21 +1,11 @@
-const fs = require('fs')
-const path = require('path')
 const withPlugins = require('next-compose-plugins')
 const optimizedImages = require('next-optimized-images')
 
 module.exports = withPlugins([optimizedImages], {
   webpack(config) {
-    //Absolute imports paths
-    const ROOT_DIR = process.cwd()
-
-    const directories = fs
-      .readdirSync(ROOT_DIR, { withFileTypes: true })
-      // Disregard hidden folders
-      .filter(file => file.isDirectory() && file.name[0] !== '.')
-      .map(file => file.name)
-
-    directories.forEach(directory => {
-      config.resolve.alias[directory] = path.join(__dirname, directory)
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader'
     })
 
     return config
